59fb1af10d3ba66659af196cebbf9e4cbba42ada
Franco <francoguardini10@gmail.com>
2022-04-30 21:36:54 -0300

    frontend socket config finished

--- public/socket.client.js
+++ public/socket.client.js
@@ -1,1 +1,24 @@
-console.log("Hi");
+const userState = document.querySelector("#user-state");
+const inputMsg = document.querySelector("#input-message");
+const btnSendMsg = document.querySelector("#send-message");
+const socket = io();
+
+btnSendMsg.addEventListener("click", (e) => {
+  e.preventDefault();
+  const message = inputMsg.value;
+  console.log(message)
+  socket.emit("send-msg", message);
+});
+
+socket.on("connect", () => {
+  console.log("connected");
+  userState.textContent = "Connected";
+  userState.classList.remove("text-danger");
+  userState.classList.add("text-success");
+});
+socket.on("disconnect", () => {
+  console.log("connected");
+  userState.textContent = "Disconnected";
+  userState.classList.remove("text-success");
+  userState.classList.add("text-danger");
+});
--- server.js
+++ server.js
@@ -5,11 +5,20 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 const httpServer = require("http").createServer(app);
-require("socket.io")(httpServer);
+const io = require("socket.io")(httpServer);
 
 app.use(express.static("public"));
 app.use(cors());
 
+io.on("connection", (socket) => {
+  console.log("Client connected");
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+  socket.on("send-msg", (payload) => {
+      console.log(payload)
+  })
+});
 httpServer.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
 );
